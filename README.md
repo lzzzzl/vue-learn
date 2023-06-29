d8d94980fad13ab8a27ee503331a9a4d2651cd64
lzzzzl <lzzzzl005@gmail.com>
2023-06-29 08:15:14 +0800

    vue lesson 6

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
-# vue-modal-project
+# Vue Learn
 
-## Project setup
-```
-npm install
-```
+- vue-modal-project
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue-reaction-timer/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start">play</button>
+  <Block />
+</template>
+
+<script>
+import Block from './components/Block.vue'
+
+export default {
+  name: 'App',
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
