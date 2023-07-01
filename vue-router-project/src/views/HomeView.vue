6d1e4683b7b378424df89525c0210c0422415e8f
StepBackLin <597023590@qq.com>
2023-07-01 23:56:25 +0800

    #8 - The Vue Router - init

---
+++ vue-router-project/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas veniam magnam commodi, corporis nam modi. Facere commodi quasi eos nobis minima enim ipsam, corporis sed ducimus qui aliquid voluptates voluptas necessitatibus sapiente tempora in nesciunt placeat nam quis dolorem cupiditate quae neque. Cumque laboriosam obcaecati, delectus consequuntur itaque nemo eum earum. Temporibus aperiam accusantium neque rerum veniam. Dolores at aliquid voluptatibus tempore amet, ad ratione odio rem error inventore odit doloribus omnis vitae asperiores repellat, corporis aut, dolore iure molestias magni! Debitis aspernatur voluptatum officiis architecto, magnam doloremque laborum numquam. Asperiores maxime excepturi pariatur tempora dignissimos sit, doloremque recusandae voluptatibus?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas veniam magnam commodi, corporis nam modi. Facere commodi quasi eos nobis minima enim ipsam, corporis sed ducimus qui aliquid voluptates voluptas necessitatibus sapiente tempora in nesciunt placeat nam quis dolorem cupiditate quae neque. Cumque laboriosam obcaecati, delectus consequuntur itaque nemo eum earum. Temporibus aperiam accusantium neque rerum veniam. Dolores at aliquid voluptatibus tempore amet, ad ratione odio rem error inventore odit doloribus omnis vitae asperiores repellat, corporis aut, dolore iure molestias magni! Debitis aspernatur voluptatum officiis architecto, magnam doloremque laborum numquam. Asperiores maxime excepturi pariatur tempora dignissimos sit, doloremque recusandae voluptatibus?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>
