f0f28265c3957c4a06dd323b441a291d3b51ef19
lzzzzl <lzzzzl005@gmail.com>
2023-07-02 16:01:10 +0800

    #10 Composition API, ref

---
+++ vue-dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name">
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'HomeView',
+  setup() {
+    console.log('setup')
+
+    const name = ref('mario')
+    const age = ref(30)
+
+    const handleClick = () => {
+      name.value = 'lzzzzl'
+      age.value = 35
+    }
+
+    return { name, age, handleClick}
+  }
+}
+</script>
