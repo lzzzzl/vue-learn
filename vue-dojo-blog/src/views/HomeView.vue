82e86f5c9de85807cdf0b72b1ea955aafae645aa
lzzzzl <lzzzzl005@gmail.com>
2023-07-02 16:22:48 +0800

    #10 - The Composition API, Finish

--- vue-dojo-blog/src/views/HomeView.vue
+++ vue-dojo-blog/src/views/HomeView.vue
@@ -1,30 +1,40 @@
 <template>
   <div class="home">
-    home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">add 1 to age</button>
-    <input type="text" v-model="name">
+    <h1>Home</h1>
+    <input type="text" v-model="search">
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { computed, ref, watch, watchEffect } from 'vue';
 
 export default {
   name: 'HomeView',
   setup() {
-    console.log('setup')
+    const search = ref('')
+    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
 
-    const name = ref('mario')
-    const age = ref(30)
+    const stopWatch = watch(search, () => {
+      console.log('watch function ran')
+    })
+
+    const stopEffect = watchEffect(() => {
+      console.log('watchEffect function ran', search.value)
+    })
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
 
     const handleClick = () => {
-      name.value = 'lzzzzl'
-      age.value = 35
+      stopWatch()
+      stopEffect()
     }
 
-    return { name, age, handleClick}
+    return { names, search, matchingNames, handleClick }
   }
 }
 </script>
